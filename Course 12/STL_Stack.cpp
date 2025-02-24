9fe78c850358dd052871cc639f9c90f47423e8f1
Ahmad Tabash <ahmad2tabash@gmail.com>
2025-02-24 22:07:03 +0300

    Swap Function In Stack

--- Course 12/STL_Stack.cpp
+++ Course 12/STL_Stack.cpp
@@ -5,24 +5,33 @@ using namespace std;
 
 int main() 
 {
-	stack<int> stkNumbers;
-	stkNumbers.push(10);
-	stkNumbers.push(20);
-	stkNumbers.push(30);
-	stkNumbers.push(40);
-	stkNumbers.push(50);
+	stack<int> stk1,stk2;
+	stk1.push(10);
+	stk1.push(20);
+	stk1.push(30);
+	stk1.push(40);
+	
+	stk2.push(50);
+	stk2.push(60);
+	stk2.push(70);
+	stk2.push(80);
 
-	cout << "Stack Size : " << stkNumbers.size() << endl;
-
-	while (!stkNumbers.empty())
+	stk1.swap(stk2);
+	//cout << "Stack Size : " << stk1.size() << endl;	
+	
+	cout << "Stack1 After Swap : " << "  ";
+	while (!stk1.empty())
 	{
-		cout << stkNumbers.top() << endl;
-
-
-		stkNumbers.pop();
+		cout << stk1.top() << " ";
+		stk1.pop();
+	}	
+	
+	cout << "\nStack2 After Swap : " << "  ";
+	while (!stk2.empty())
+	{
+		cout << stk2.top() << " ";
+		stk2.pop();
 	}
-
-
 
 	return 0;
 }
