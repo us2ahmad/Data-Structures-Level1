9192be10e815538c0af6a9bb42e10866b3c5038e
Ahmad Tabash <ahmad2tabash@gmail.com>
2025-02-24 22:46:37 +0300

    STL Queue

--- Course 12/STL_Stack.cpp
+++ Course 12/STL_Stack.cpp
@@ -1,37 +1,37 @@
-#include <iostream>
-#include <stack>
-
-using namespace std;
-
-int main() 
-{
-	stack<int> stk1,stk2;
-	stk1.push(10);
-	stk1.push(20);
-	stk1.push(30);
-	stk1.push(40);
-	
-	stk2.push(50);
-	stk2.push(60);
-	stk2.push(70);
-	stk2.push(80);
-
-	stk1.swap(stk2);
-	//cout << "Stack Size : " << stk1.size() << endl;	
-	
-	cout << "Stack1 After Swap : " << "  ";
-	while (!stk1.empty())
-	{
-		cout << stk1.top() << " ";
-		stk1.pop();
-	}	
-	
-	cout << "\nStack2 After Swap : " << "  ";
-	while (!stk2.empty())
-	{
-		cout << stk2.top() << " ";
-		stk2.pop();
-	}
-
-	return 0;
-}
+//#include <iostream>
+//#include <stack>
+//
+//using namespace std;
+//
+//int main() 
+//{
+//	stack<int> stk1,stk2;
+//	stk1.push(10);
+//	stk1.push(20);
+//	stk1.push(30);
+//	stk1.push(40);
+//	
+//	stk2.push(50);
+//	stk2.push(60);
+//	stk2.push(70);
+//	stk2.push(80);
+//
+//	stk1.swap(stk2);
+//	//cout << "Stack Size : " << stk1.size() << endl;	
+//	
+//	cout << "Stack1 After Swap : " << "  ";
+//	while (!stk1.empty())
+//	{
+//		cout << stk1.top() << " ";
+//		stk1.pop();
+//	}	
+//	
+//	cout << "\nStack2 After Swap : " << "  ";
+//	while (!stk2.empty())
+//	{
+//		cout << stk2.top() << " ";
+//		stk2.pop();
+//	}
+//
+//	return 0;
+//}
